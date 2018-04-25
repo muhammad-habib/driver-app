bcf8e9ee45001be6f9d809b8b764d69ce98ea014
Amr Elsayed <amr@zadfresh.com>
2018-04-25 14:13:44 +0300

    api docs

--- app/Http/Controllers/Driver/AuthController.php
+++ app/Http/Controllers/Driver/AuthController.php
@@ -37,12 +37,95 @@ class AuthController extends Controller
         }
     }
 
+    
     /**
     *	Driver Login
     *
     *	@param Request $request
     * 	@return Json
     */
+
+    /**
+     * @SWG\Post(
+     *     path="/driver/login",
+     *     summary="Driver Login",
+     *     tags={"Driver"},
+     *     description="Driver Login with Username and Password",
+     *     operationId="login",
+     *     produces={"application/json"},
+     *     @SWG\Parameter(
+     *         name="username",
+     *         in="formData",
+     *         description="Driver Username",
+     *         required=true,
+     *         type="string",
+     *     ),
+     *     @SWG\Parameter(
+     *         name="password",
+     *         in="formData",
+     *         description="Driver Password",
+     *         required=true,
+     *         type="string",
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Successful operation returns driver token",
+     *         @SWG\Schema(
+     *              @SWG\Property(
+     *                      property="message",
+     *                      type="string",
+     *              ),
+     *              @SWG\Property(
+     *                      property="data",
+     *                      type="string",
+     *              )
+     *         )
+     *     ),
+     *     @SWG\Response(
+     *         response="400",
+     *         description="Validation Errors",
+     *         @SWG\Schema(
+     *              @SWG\Property(
+     *                      property="message",
+     *                      type="string",
+     *                      default="Fields are invalid",
+     *              ),
+     *              @SWG\Property(
+     *                      property="details",
+     *                      type="string",
+     *              )
+     *         ),
+     *     ),
+     *     @SWG\Response(
+     *         response="401",
+     *         description="username and password are not correct",
+     *         @SWG\Schema(
+     *              @SWG\Property(
+     *                      property="message",
+     *                      type="string",
+     *              )
+     *         ),
+     *     ),
+     *     @SWG\Response(
+     *         response="500",
+     *         description="SERVER ERROR",
+     *         @SWG\Schema(
+     *              @SWG\Property(
+     *                      property="message",
+     *                      type="string",
+     *                      default="Server Error",
+     *              ),
+     *              @SWG\Property(
+     *                      property="details",
+     *                      type="string",
+     *              )
+     *         )
+     *     ),
+     *     security={
+     *       {"default": {}}
+     *     }
+     * )
+     */
     public function login(Request $request)
     {
 
@@ -56,44 +139,47 @@ class AuthController extends Controller
         if ($validator->fails()){
             return ValidationError::handle($validator);
         }
+        
+        try{
 
-        // get driver from database from it's username
-    	$driver = Driver::where('username',  $request->get('username'))->first();
+            // get driver from database from it's username
+            $driver = Driver::where('username',  $request->get('username'))->first();
 
-    	// return failed message if driver doesn't exist
-        if(!$driver){
-        	return response()->json([
-            	'success' => false,
-                'status_code' => 400,
-                'message' => trans('driver.login.error.username')
-            ]);
+            // return failed message if driver doesn't exist
+            if(!$driver){
+                return response()->json([
+                    'message' => trans('driver.login.error.username')
+                ], 401);
+            }
+
+            // comparing hashed password & if not equal to driver password in db return error message
+            if (!Hash::check($request->get('password'), $driver->password))
+            {
+                return response()->json([
+                    'message' => trans('driver.login.error.password')
+                ], 401);
+            }
+            
+            // generate token
+            $customClaims = ['sub' => $driver->id, 'app' => 'driver'];
+            $payload = JWTFactory::make($customClaims);
+            $token = JWTAuth::encode($payload);
+
+            // store token in db
+            $driver->token = $token->get();
+            $driver->save();
+
+            // return driver token
+            return response()->json([
+                'message' => trans('driver.login.success'),
+                'data' =>  $driver->token,
+            ], 200);
+
+        }catch (\Exception $e){
+
+            return ServerError::handle($e);
         }
 
-        // comparing hashed password & if not equal to driver password in db return error message
-		if (!Hash::check($request->get('password'), $driver->password))
-		{
-		    return response()->json([
-            	'success' => false,
-                'status_code' => 400,
-                'message' => trans('driver.login.error.password')
-            ]);
-		}
-        
-        // generate token
-        $customClaims = ['sub' => $driver->id, 'app' => 'driver'];
-        $payload = JWTFactory::make($customClaims);
-        $token = JWTAuth::encode($payload);
-
-        // store token in db
-        $driver->token = $token->get();
-        $driver->save();
-
-        // return success and driver token
-        return response()->json([
-            'success' => true,
-            'status' => 200,
-            'token' =>  $driver->token,
-        ]);
 
     }
 
@@ -103,6 +189,51 @@ class AuthController extends Controller
     *   @param Request $request
     *   @return Json
     */
+
+        /**
+     * @SWG\Post(
+     *     path="/driver/logout",
+     *     summary="Driver Logout",
+     *     tags={"Driver"},
+     *     operationId="logout",
+     *     produces={"application/json"},
+     *     @SWG\Parameter(
+     *         name="Authorization",
+     *         in="header",
+     *         description="Driver token",
+     *         required=true,
+     *         type="string",
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Driver Logged out successfully",
+     *         @SWG\Schema(
+     *              @SWG\Property(
+     *                      property="message",
+     *                      type="string",
+     *              ),
+     *         )
+     *     ),
+     *     @SWG\Response(
+     *         response="500",
+     *         description="SERVER ERROR",
+     *         @SWG\Schema(
+     *              @SWG\Property(
+     *                      property="message",
+     *                      type="string",
+     *                      default="Server Error",
+     *              ),
+     *              @SWG\Property(
+     *                      property="details",
+     *                      type="string",
+     *              )
+     *         )
+     *     ),
+     *     security={
+     *       {"default": {}}
+     *     }
+     * )
+     */
     public function logout(Request $request)
     {
         $token = $request->header('Authorization');
@@ -112,17 +243,13 @@ class AuthController extends Controller
             Driver::where('token', $token)->update(['token' => null]);
             
             return response()->json([
-                'success' => true,
-                'status' => 200,
                 'message' => trans('driver.logout_success')
-            ]);
+            ], 200);
 
         }catch (\Exception $e){
 
             return ServerError::handle($e);
         }
-        
-
         
     }
 
--- resources/lang/en/driver.php
+++ resources/lang/en/driver.php
@@ -5,7 +5,8 @@ return [
 		'error' => [
 			'username' => 'Driver does not exist in our records',
 			'password' => 'Username and Password does not match'
-		]
+		],
+		'success' => 'Driver login success'
 	],
 	'logout_success' => 'Logged out successfully'
 ];
