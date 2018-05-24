a24c3123ee8cc2dd46b13ea64f7a926b2c6e8905
Amr Elsayed <amr@zadfresh.com>
2018-05-24 15:05:16 +0300

    driver start / end shift

---
+++ database/migrations/2018_05_23_151101_create_driver_shifts.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDriverShifts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('driver_shifts', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('driver_id')->unsigned()->index();      
+            $table->time('start_at');
+            $table->time('end_at')->nullable();
+
+            $table->timestamps();
+        });
+
+        Schema::table('drivers', function(Blueprint $table){
+            $table->smallInteger('in_duty')->default(0);
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('driver_shifts');
+
+        Schema::table('drivers', function(Blueprint $table){
+            $table->dropColumn('in_duty');
+        });
+    }
+}
