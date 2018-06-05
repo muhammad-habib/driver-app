e0b2a4cc66e20f9e628d501eb4bc0488abfc396e
Anoos <bloggersb92@gmail.com>
2018-06-05 16:19:05 +0200

    Auto stash before merge of "development" and "origin/development"

--- routes/api.php
+++ routes/api.php
@@ -84,6 +84,6 @@ Route::post('/push-repo', 'Development\DeploymentController@pullDevelopmentBranc
 // Testing APIs
 Route::get('users', 'Pet@index');
 Route::middleware(['docs', 'test'])->get('users', 'Pet@index');
-Route::get('test-d', function () {
+Route::get('test-d1', function () {
     return 123;
 });
